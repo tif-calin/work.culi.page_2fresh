fb3c50683faffbcaf7b7de9df5cb47b918c77b1e
culi🌸 <culitif@tuta.io>
2023-02-16 00:21:42 -0800

    adds basic styling and info to home page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,22 +1,48 @@
 import StyledHead from "../components/StyledHead.tsx";
 
+const sectionContentClass = "p-4 mx-auto max-w-screen-md";
+
 export default function Home() {
   return (
     <>
       <StyledHead>
-        <title>Fresh App</title>
+        <title>Culi Tif — portfolio</title>
       </StyledHead>
+      <style>
+        {`
+          main section > div {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: calc(100vh - 6rem);
+          }
+        `}
+      </style>
       <main class="flex-grow">
-        <section class="p-4 mx-auto max-w-screen-md">
-          <h1>Hi, I'm Culi!</h1>
-          <p class="my-6">
+        <section>
+          <div class={sectionContentClass}>
+            <h1 class="font-serif text-7xl font-black">
+              Hi<span class="text-red-500">,</span>{" "}
+              I<span class="text-red-500">'</span>m
+              Culi<span class="text-red-500">!</span>
+            </h1>
+            <p class="text-2xl">
             full-stack software engineer
           </p>
+            <p class="my-10">
+              email, linkedin, github
+            </p>
+          </div>
         </section>
       </main>
-      <footer class="p-4 mx-auto max-w-screen-md shrink flex gap-1 items-center">
-        <span class="rotate-180 translate-y-[1.2px]">&copy;</span>
-        <span>steal this</span>
+      <footer class="p-4 mx-auto max-w-screen-md flex-shrink flex gap-1 items-center">
+        <span class="rotate-180 translate-y-[1.5px]">&copy;</span>
+        <span>
+          <a href="https://github.com/tif-calin/work.culi.page_2fresh">
+            steal this
+          </a>
+        </span>
       </footer>
     </>
   );
